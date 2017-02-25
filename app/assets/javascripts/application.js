afb9b76086474ed3db53386d29fd3a8532d1b06d
Hector Miranda <marcetux@gmail.com>
2017-02-24 19:36:27 -0800

    new header bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,17 @@ document.addEventListener("turbolinks:load", function(){
     plugins: "codesample image media link code",
     toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
   });
+
+      $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
+		event.preventDefault();
+		$('body').toggleClass('navbar-more-show');
+		if ($('body').hasClass('navbar-more-show'))	{
+			$('a[href="#navbar-more-show"]').closest('li').addClass('active');
+		}else{
+			$('a[href="#navbar-more-show"]').closest('li').removeClass('active');
+		}
+		return false;
+	});
 })
+
+
