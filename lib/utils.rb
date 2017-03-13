42d92b9e2afd85d63ff823f2e9e7a5e24165d4b3
Hector Miranda <marcetux@gmail.com>
2017-03-13 07:24:06 -0700

    adding binary search recursive

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,7 +1,4 @@
 class Utils
-def info 
-    puts "np"
-end
 
 def largest_number arr
     largest_number = 0
@@ -9,6 +6,7 @@ def largest_number arr
     arr.each do |i|
         largest_number = i if i > largest_number
     end
+
     return largest_number
 end
 
@@ -45,5 +43,51 @@ def binary_search arr, target
     return -1
 end
 
+
+# - if array has 1 element, return that element
+# - If array has 2 elements, return the larger one
+# - Divide the array into 2 parts
+# - Find the largest number in each of the two halves ()
+# - Compare the largest numbers and return the larget one
+def largest_number_rec arr
+    return arr[0] if arr.length == 1
+
+    if arr.length == 2
+        return arr[0] > arr[1] ? arr[0]: arr[1]
+    end
+
+    midpoint = (arr.lenght - 1)/2
+
+    num_1 = largest_number_rec(arr[0..midpoint])
+    num_2 = largest_number_rec(arr[(midpoint+1)..(arr.lenght-1)])
+
+    return num_1>num_2 ? num_1 : num_2
+end
+
+# Binary search recursive
+# - set the minimum index as 0
+# - set the maximum idnex as the last index of the array
+# - If the minimum index is higher than the maximum index, then the element is not in the array
+# - Get the midpoint of the array by adding max + min /2 and rounding down
+# - perform binary search on the lower half of the array
+# - perform binary search on the higher half of the array
+# - if the element at the midpoint is the target
+#   return that index
+#
+# sorted_array = [1,2,3,4,5,6,7,8,10,23,33,40,50,60,80]
+
+def binary_search_rec arr, target, min_index = 0, max_index = arr.length-1
+    return -1 if min_index > max_index
+
+    midpoint = (max_index + min_index)/2
+
+    if arr[midpoint] > target
+        binary_search_rec(arr, target, 0, midpoint-1)
+    elsif arr[midpoint] < target
+        binary_search_rec(arr, target, midpoint+1, arr.length-1)
+    else
+        return midpoint
+    end
+end
 
 end
