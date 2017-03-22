264a89056d85eeb3651523621b2d7edec6fc0687
Hector Miranda <marcetux@gmail.com>
2017-03-21 18:21:01 -0700

    adding utilities

--- app/assets/javascripts/sandbox.js
+++ app/assets/javascripts/sandbox.js
@@ -2,3 +2,26 @@ function start(){
     console.log("Starting...");
 
 }
+
+
+function floor(diameter){
+    var radius = diameter/2;
+    var floorArea = Math.PI * (radius * radius);
+    return floorArea; 
+}
+
+function walls(diameter, height){
+    var radius = diameter/2;
+    var circumference = 2 * Math.PI + radius
+    var wallArea = circumference * height;
+    return wallArea;
+}
+
+function materialsNeeded(){
+    var d = document.getElementById("diameter").value;
+    var h = document.getElementById("height").value;
+    var carpetNeeded = Math.ceil(floor(d));
+    var paintNeeded = Math.ceil(walls(d, h));
+    console.log(carpetNeeded);
+    console.log(paintNeeded);
+}
